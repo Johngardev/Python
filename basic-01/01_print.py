6ef4d9c6677154d6d9bbecb82554592a95a1fedc
Johngardev <johngarlo@icloud.com>
2025-01-15 12:58:06 -0500

    add new files

---
+++ basic-01/01_print.py
@@ -0,0 +1,10 @@
+print("hola mundo, Johngardev")
+
+print('hola mundo, Johngardev comillas simples')
+
+print("Hola", "python")
+
+print("Hola", "python", sep="-")
+
+print("Hola", "python", end="***")
+print("otra linea")
