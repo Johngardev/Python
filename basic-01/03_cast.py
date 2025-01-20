04ce47cfeea62115debbb7d13ce9aa7d3b73eb57
Johngardev <johngarlo@icloud.com>
2025-01-20 18:37:36 -0500

    Exercises with solutions

--- basic-01/03_cast.py
+++ basic-01/03_cast.py
@@ -6,7 +6,7 @@
 print("Conversion de tipos")
 
 #print(int("10") + 2)
-#print("100" + str(2))
+print("100" + str(2))
 #print(10 + float("2.5"))
 #print(10 + int(2.5))
 #print(10 + int(float("2.5")))
--- basic-01/exercises.py
+++ basic-01/exercises.py
@@ -6,7 +6,9 @@
 print("\nEjercicio 1: Imprimir mensajes")
 print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
 
-### Completa aquí
+name = input("Ingresa tu nombre: ")
+city = input("Ingresa tu ciudad: ")
+print("Name: {name}", "City: {city}", sep="\n")
 
 print("--------------")
 
@@ -18,7 +20,11 @@ c = "Hola mundo"
 d = True
 e = None
 
-### Completa aquí
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
 
 print("--------------")
 
@@ -26,7 +32,8 @@ print("\nEjercicio 3: Casting de tipos")
 print("Convierte la cadena \"12345\" a un entero y luego a un float.")
 print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
 
-### Completa aquí
+print(float(int("12345")))
+print(int(3.99))
 
 print("--------------")
 
@@ -36,7 +43,11 @@ print("Usa f-strings para imprimir una presentación.")
 
 # "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
 
-### Completa aquí
+name = input("Ingresa tu nombre: ")
+age = input("Ingresa tu edad: ")
+height = input("Ingresa tu altura: ")
+
+print(f"Hola! Me llamo {name} y tengo {age} años, mido {height} metros")
 
 print("--------------")
 
@@ -45,3 +56,6 @@ print("1. Crea una variable con el número PI (sin asignar una variable)")
 print("2. Redondea el número con round()")
 print("3. Haz la división entera entre el número que te salió y el número 2")
 print("4. El resultado debería ser 1")
+
+print(round(3.14159))
+print(3 // 2)
